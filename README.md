b8ae389985a75b1e4ab4671dff5d62f98fd2301d
Austin Parker <austinlparker@gmail.com>
2020-06-02 15:32:44 -0400

    add helm, client, etc.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Spring + Vue.JS SPA OpenTelemetry Example
+
+This is a sample application demonstrating the usage of OpenTelemetry with Spring and Vue.JS in Kubernetes.
---
+++ client/src/config.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-unused-vars */
+const config = (() => {
+  return {
+    "VUE_APP_ENV_Server": "http://localhost:9000",
+  };
+})();
+/* eslint-enable no-unused-vars */
