3ecc1ef6418e3a3d3f14083e41188c8ab11e6055
Austin Parker <austin@lightstep.com>
2020-07-28 11:36:28 -0400

    updated for new versions

--- client/src/tracer.js
+++ client/src/tracer.js
@@ -4,8 +4,7 @@ import { XMLHttpRequestPlugin } from '@opentelemetry/plugin-xml-http-request';
 import { UserInteractionPlugin } from '@opentelemetry/plugin-user-interaction';
 import { DocumentLoad } from '@opentelemetry/plugin-document-load';
 import { ZoneContextManager } from '@opentelemetry/context-zone';
-import { CollectorExporter } from '@opentelemetry/exporter-collector';
-import { HttpTraceContext  } from '@opentelemetry/core';
+import { CollectorTraceExporter } from '@opentelemetry/exporter-collector';
 
 /* eslint-disable no-undef */
 const collectorUrl = config.VUE_APP_ENV_Collector || 'http://localhost:30011/v1/trace'
@@ -13,10 +12,10 @@ const serverBaseUrl = config.VUE_APP_ENV_ServerBase || 'localhost:30005'
 const baseLocation = window.location.hostname || 'localhost'
 /* eslint-enable no-undef */
 
-const exporter = new CollectorExporter({
+const exporterOptions = {
   serviceName: 'todo-client',
   url: collectorUrl,
-});
+};
 
 const providerWithZone = new WebTracerProvider({
   plugins: [
@@ -24,17 +23,14 @@ const providerWithZone = new WebTracerProvider({
     new UserInteractionPlugin(),
     new XMLHttpRequestPlugin({
       ignoreUrls: [new RegExp(`/${baseLocation}:8090/sockjs-node/`)],
-      propagateTraceHeaderCorsUrls: [
-        new RegExp(`/${serverBaseUrl}/`),
-      ],
+      propagateTraceHeaderCorsUrls: new RegExp(`/${serverBaseUrl}/`),
     }),
   ],
 });
 
 providerWithZone.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
-providerWithZone.addSpanProcessor(new SimpleSpanProcessor(new CollectorExporter(exporter)));
+providerWithZone.addSpanProcessor(new SimpleSpanProcessor(new CollectorTraceExporter(exporterOptions)));
 
 providerWithZone.register({
-  contextManager: new ZoneContextManager(),
-  propagator: new HttpTraceContext(),
+  contextManager: new ZoneContextManager()
 });
